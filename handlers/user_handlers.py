abb85ceabbe27a3bef879264e2060edf116ec390
artek22 <ya.geffixe@yandex.ru>
2023-08-09 17:50:46 +0300

    other roles checking before assign

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -10,12 +10,20 @@ ROLE = {'methodist': [], 'councelor': [], 'kid': []}
 # Этот хэндлер срабатывает на кодовое слово и присваивает роль методиста
 @router.message(Text(text='methodist'))
 async def process_start_command(message: Message):
-    ROLE['methodist'].append(message.chat.id)
-    await message.answer(text='Вы получили права доступа методиста.')
+    # Проверяем не получал ли юзер ранее права вожатого
+    if message.chat.id not in ROLE['councelor']:
+        ROLE['methodist'].append(message.chat.id)
+        await message.answer(text='Вы получили права доступа методиста.')
+    else:
+        await message.answer(text='У вас права доступа вожатого.')
 
 
 # Этот хэндлер срабатывает на кодовое слово и присваивает роль вожатого
 @router.message(Text(text='councelor'))
 async def process_start_command(message: Message):
-    ROLE['councelor'].append(message.chat.id)
-    await message.answer(text='Вы получили права доступа вожатого.')
+    # Проверяем не получал ли юзер ранее права методиста
+    if message.chat.id not in ROLE['methodist']:
+        ROLE['councelor'].append(message.chat.id)
+        await message.answer(text='Вы получили права доступа вожатого.')
+    else:
+        await message.answer(text='У вас права доступа методиста.')
