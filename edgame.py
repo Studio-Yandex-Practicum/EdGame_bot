7948ac21b3e065332eaaf3f41846058591705067
artek22 <ya.geffixe@yandex.ru>
2023-08-09 15:19:19 +0300

    Main file edgame.py creating

--- edgame.py
+++ edgame.py
@@ -1,6 +1,8 @@
 import asyncio
 import logging
 
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
 from auth.auth import start_bot
 
 # Инициализируем логгер
@@ -17,6 +19,18 @@ async def main():
 
     # Выводим в консоль информацию о начале запуска бота
     logger.info('Starting bot')
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Инициализируем бот и диспетчер
+    bot: Bot = Bot(token=config.tg_bot.token,
+                   parse_mode='HTML')
+    dp: Dispatcher = Dispatcher()
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
