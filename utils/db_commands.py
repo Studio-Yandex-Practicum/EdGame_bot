47f1beb35851d003fd2a18717546276fbdbf42e6
artek22 <ya.geffixe@yandex.ru>
2023-09-04 17:17:44 +0300

    Survey

--- utils/db_commands.py
+++ utils/db_commands.py
@@ -7,21 +7,21 @@ from db.engine import session
 from db.models import Achievement, AchievementStatus, User
 
 
-def register_user(message):
-    name = message.chat.first_name if message.chat.first_name else None
-    user = User(
-        id=int(message.chat.id), name=name, role="kid", language="RU", score=0,
-        group=0
-    )
-
-    session.add(user)
-
-    try:
-        session.commit()
-        return True
-    except IntegrityError:
-        session.rollback()  # откатываем session.add(user)
-        return False
+# def register_user(message):
+#     name = message.chat.first_name if message.chat.first_name else None
+#     user = User(
+#         id=int(message.chat.id), name=name, role="kid", language="RU", score=0,
+#         group=0
+#     )
+#
+#     session.add(user)
+#
+#     try:
+#         session.commit()
+#         return True
+#     except IntegrityError:
+#         session.rollback()  # откатываем session.add(user)
+#         return False
 
 
 def select_user(user_id) -> User:
