5b2ab850765116bcd060c4f74328f4ace466f8ac
Anton <A60874022@yandex.ru>
2023-10-22 17:19:46 +0500

    child

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -59,7 +59,7 @@ def profile_keyboard(language) -> ReplyKeyboardMarkup:
     write_to_councelor = KeyboardButton(text=buttons["write_to_councelor"])
     help_button = KeyboardButton(text=buttons["help"])
     join_team = KeyboardButton(text=buttons["join_team"])
-
+    
     keyboard = [
         [available_achievements, current_achievements],
         [reviewed_achievements, join_team],
